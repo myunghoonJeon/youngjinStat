569c68a5a0dcfa7b058e6978778a130b0529e998
jmh <takoowang@gmail.com>
2014-02-23 00:25:39 +0900

    gogo

--- youngjinStat/src/InventoryInput.java
+++ youngjinStat/src/InventoryInput.java
@@ -4,7 +4,7 @@ import javax.swing.JPanel;
 public class InventoryInput {
 	public JPanel getInputPanel(){
 		JPanel jp = new JPanel();
-				
+		System.out.println("Inventory Input");
 		return jp;
 	}
 }
